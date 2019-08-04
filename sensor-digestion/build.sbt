ed68c19e265fd4b44640a2b9587e512a1c45e2e0
Joël Vallone <joel.vallone@gmail.com>
2019-08-04 17:46:54 +0200

    CQL query for raw sensor data access.

--- sensor-digestion/build.sbt
+++ sensor-digestion/build.sbt
@@ -4,8 +4,12 @@ version := "0.1"
 
 scalaVersion := "2.12.8"
 
+resolvers += Resolver.mavenLocal
+
 libraryDependencies += "junit" % "junit" % "4.10" % Test
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "2.4.3",
-  "org.apache.spark" %% "spark-sql" % "2.4.3"
+  "org.apache.spark" %% "spark-sql" % "2.4.3",
+
+  "org.digitalpanda" % "backend.api" % "0.1.0"
 )
--- sensor-digestion/src/main/scala/org/digitalpanda/bigdata/sensor/SensorDigestion.scala
+++ sensor-digestion/src/main/scala/org/digitalpanda/bigdata/sensor/SensorDigestion.scala
@@ -2,19 +2,36 @@ package org.digitalpanda.bigdata.sensor
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.SparkConf
+import org.digitalpanda.backend.data.SensorMeasureType.TEMPERATURE
+import org.digitalpanda.backend.data.history.HistoricalDataStorageHelper.getRangeSelectionCqlQueries
+import org.digitalpanda.backend.data.history.HistoricalDataStorageSizing.SECOND_PRECISION_RAW
 
 object SensorDigestion {
+
 
   @transient lazy val conf: SparkConf = new SparkConf()
     .setMaster("local")
     .setAppName("SensorDigestion")
     .set("spark.driver.bindAddress", "127.0.0.1")
 
-  @transient lazy val sc: SparkSession = SparkSession.builder()
+  @transient lazy val session: SparkSession = SparkSession.builder()
     .config(conf)
     .getOrCreate()
 
   def main(args: Array[String]): Unit = {
-    println("Started SensorDigestion")
+    //val lines = session.read.textFile("src/main/resources/sensor_measure_history_seconds.csv")
+    print(s"$getRawSensorMeasureCqlExampleQuery")
   }
+
+  def getRawSensorMeasureCqlExampleQuery: String = {
+    val now = System.currentTimeMillis()
+    val yesterday = now - 24 * 3600 * 1000
+    getRangeSelectionCqlQueries(
+      "server-room",
+      TEMPERATURE,
+      SECOND_PRECISION_RAW,
+      yesterday,
+      now).get(0)
+  }
+
 }
